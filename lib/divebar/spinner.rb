5fb2c2bb593f3136d5c82b84c4dd908a44aec6e7
Thomas Hurst <tom@hur.st>
2018-12-06 21:53:01 +0000

    Add a simple Spinner type

---
+++ lib/divebar/spinner.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Divebar
+  # A cycling progress spinner indicator
+  #
+  # @example
+  #   Spinner.new("1234").cycle { |spin| print("\r#{spin}") } # 1\r2\r3\r4\r1...
+  class Spinner
+    include Enumerable
+
+    # The set of states this spinner can be in
+    attr_reader :states
+
+    # The character width of this spinner
+    attr_reader :width
+
+    # Create a spinner renderer that cycles between a set of predefined states.
+    # If the list of states is not of equal size, smaller states will be left-
+    # padded so the spinner is of constant width.
+    #
+    # A String of single characters is also supported.
+    #
+    # @param states [String, Array<String>] an array of spinner states
+    # @return [Spinner]
+    def initialize(states = ".:oO*")
+      states = states.chars if states.is_a?(String)
+      raise ArgumentError, "must specify at least two states" if states.size < 2
+
+      @width = states.max(&:size)
+      @states = states.map { |state| state.ljust(@width, ' ').freeze }
+    end
+
+    # Return the given spinner state.
+    #
+    # @param pos [Integer] the current render position, 0..size
+    # @return [String] frozen representation of the bar
+    def [](pos)
+      @states[pos]
+    end
+
+    # Iterate over each state of the bar
+    #
+    # @yieldparam [String] bar a frozen String of the spinner
+    # @return [nil, Enumerator]
+    def each
+      return enum_for(__method__) unless block_given?
+
+      states.each do |state|
+        yield state
+      end
+    end
+  end
+end
